a2d003244f11c00946a7a6999969a1ccc7b1f4d5
LowGcifer <LowGcifer@gmail.com>
2021-08-29 19:01:40 -0400

    making a list

---
+++ public/js/homepage.js
@@ -0,0 +1,6 @@
+$(function () {
+  var artists = $("#artist-list")
+    .selectmenu()
+    .selectmenu("menuWidget")
+    .addClass("overflow");
+});
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,30 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // const username = document.querySelector("#username-signup").value.trim();
-    const username = document.querySelector("#username-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-    const state = document.querySelector("#state-signup").value.trim();
-  
-    if (username && password && state) {
-      
-      const response = await fetch("/api/users", {
-        method: "POST",
-        body: JSON.stringify({ username, password, state }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        document.location.replace("/");
-      } else {
-        alert("Failed to sign up.");
-      }
-    } 
-  };
-  
-  document
-    .querySelector(".signup-form")
-    .addEventListener("submit", signupFormHandler);
-  
+  event.preventDefault();
+
+  // const username = document.querySelector("#username-signup").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  const state = document.querySelector("#state-signup").value.trim();
+
+  if (username && password && state) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password, state }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
+
+$(function () {
+  $("#state-signup").selectmenu();
+});
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);

b9631a06f1bbd3653711be5d6a19886361b40bf4
LowGcifer <LowGcifer@gmail.com>
2021-08-30 01:10:56 -0400

    Did a bunch of stuff

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const artistRoutes = require('./artist-routes');
-const userRoutes = require('./user-routes');
+const router = require("express").Router();
+const artistRoutes = require("./artist-routes");
+const userRoutes = require("./user-routes");
 
-router.use('/users', userRoutes);
-router.use('/artist', artistRoutes);
+router.use("/users", userRoutes);
+router.use("/artists", artistRoutes);
 
 module.exports = router;
